cc3833f38c5665e7c42fafa8b643ee5092f5adb2
Aaron Neiderhiser <anneiderhiser@gmail.com>
2026-08-28 14:11:43 -0600

    Add explicit package version contract for dbt v2

--- macros/load_semantic_layer_seed.sql
+++ macros/load_semantic_layer_seed.sql
@@ -1,7 +1,7 @@
 {% macro load_semantic_layer_seed(object_name) %}
   {{ return(the_tuva_project.load_package_seed(
-      'semantic_layer',
       'semantic-layer',
+      semantic_layer.get_semantic_layer_package_version(),
       object_name,
       true,
       true,
